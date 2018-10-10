0abd6bc0e2d83f0257e227bd814260e541d82013
durga@prasadSlinfy <durga.prasad@slinfy.com>
2018-10-10 11:36:21 +0530

    Initial Commit

---
+++ SampleVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleVideo
+//
+//  Created by Durga Prasad on 10/10/18.
+//  Copyright © 2018 Solitaire Infotech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
